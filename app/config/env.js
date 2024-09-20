39ff36016d9576b7876580025d5a0eb3639ba7ca
JonatanAjanelG <jonatanajanel2002gmail.com>
2024-09-20 16:25:38 -0600

    cambio de host

--- app/config/env.js
+++ app/config/env.js
@@ -3,8 +3,8 @@ const env = {
     database: 'umg_salama_16431_wvn6',
     username: 'umg_salama_16431_wvn6_user',
     password: 'lkL0FH0bSrkd49fc6q7ybJGln4svvliJ',
-    host: 'dpg-crmgum23esus73fsvheg-a.oregon-postgres.render.com', //este es el host externo
-    //host: 'dpg-crmgum23esus73fsvheg-a' usar este para subir el proyecto a render final
+    //host: 'dpg-crmgum23esus73fsvheg-a.oregon-postgres.render.com', //este es el host externo
+    host: 'dpg-crmgum23esus73fsvheg-a', //usar este para subir el proyecto a render final
     dialect: 'postgres',
     
     pool: {
